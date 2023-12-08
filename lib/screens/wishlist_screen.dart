968b0eaaadc1370cbd61786aadaee9a65e634991
jdtiu322 <jdtiu322@gmail.com>
2023-12-09 05:44:44 +0800

    wishlists

--- lib/screens/wishlist_screen.dart
+++ lib/screens/wishlist_screen.dart
@@ -10,6 +10,8 @@ class WishlistScreen extends StatefulWidget {
 class _WishlistScreenState extends State<WishlistScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text("My Wishlists")),
+    );
   }
 }
